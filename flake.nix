54a78656ee80d065853fbbd71717a61c1295dfc0
Name <namesexistsinemails@gmail.com>
2024-05-26 21:34:04 +0100

    Feat: Add Nix and last update to css

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Catppuccin userstyles";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            deno
+            lessc
+          ];
+        };
+      }
+    );
+}
